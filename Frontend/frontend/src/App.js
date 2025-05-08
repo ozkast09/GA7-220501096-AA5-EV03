f75ef78495e2d883cab430e2b21b2f8ae80b1fb6
oscar <oscar.javier741@gmail.com>
2025-05-07 19:36:13 -0500

    se modigfica archivo App.js

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router,Route,Routes } from "react-router-dom";
+import Layout from './pages/StockPage';
+import CreateProductPage from "./pages/CreateProductPage";
+import StockPage from "./pages/StockPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function App(){
+  return(
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout><StockPage/></Layout>}/>
+        <Route path="/stock" element={<Layout><CreateProductPage/></Layout>}/>
+        <Route path="/crear-producto" element={<Layout><CreateProductPage/></Layout>}/>
+      </Routes>
+    </Router>
   );
 }
 
